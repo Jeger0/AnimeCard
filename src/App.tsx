22e3d3b200d20df923d73cc289bcac37c6e0d426
Jeger <563keyboard@gmail.com>
2025-12-18 14:59:41 +0100

    added loading and error

--- src/App.tsx
+++ src/App.tsx
@@ -9,25 +9,40 @@ type Anime = {
 };
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
   const [animeList, setAnimeList] = useState<Anime[]>([]);
 
   useEffect(() => {
+    setLoading(true);
     fetch("https://api.jikan.moe/v4/anime")
       .then((res) => res.json())
       .then((data) => {
         setAnimeList(data.data);
+      })
+      .catch((err) => {
+        setError(err.message);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   }, []);
 
   return (
     <div>
-      {animeList.map((anime) => (
-        <AnimeCard
-          key={anime.mal_id}
-          title={anime.title}
-          synopsis={anime.synopsis}
-        />
-      ))}
+      {error && <p>{error}</p>}
+
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        animeList.map((anime) => (
+          <AnimeCard
+            key={anime.mal_id}
+            title={anime.title}
+            synopsis={anime.synopsis}
+          />
+        ))
+      )}
     </div>
   );
 }
