c6e7081cd01c83f12e2de79aa55bd727db462b46
Jeger <563keyboard@gmail.com>
2025-12-23 02:33:00 +0100

    changed to destructure

--- src/assets/components/ThemeToggle.tsx
+++ src/assets/components/ThemeToggle.tsx
@@ -3,13 +3,13 @@ type ThemeToggleProps = {
   onToggle: () => void;
 };
 
-function ThemeToggle(props: ThemeToggleProps) {
+function ThemeToggle({ dark, onToggle }: ThemeToggleProps) {
   return (
     <button
-      onClick={props.onToggle}
+      onClick={onToggle}
       className="border p-2 rounded mb-4 transition dark:text-white dark:border-white"
     >
-      {props.dark ? "Switch to Light Mode" : "Switch to Dark Mode"}
+      {dark ? "Switch to Light Mode" : "Switch to Dark Mode"}
     </button>
   );
 }
